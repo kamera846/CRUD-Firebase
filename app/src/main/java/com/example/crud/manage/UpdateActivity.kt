ebb48548c5721fca025d75312998c64d75dbeaa0
Rafliram <keerouk.ink@gmail.com>
2020-07-16 23:59:33 +0700

    (+) Manage Layout

---
+++ app/src/main/java/com/example/crud/manage/UpdateActivity.kt
@@ -0,0 +1,12 @@
+package com.example.crud.manage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.crud.R
+
+class UpdateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update)
+    }
+}
