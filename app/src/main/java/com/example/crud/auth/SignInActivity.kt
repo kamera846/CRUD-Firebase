12554e0fe4e87710c0e0507dda1f64f6e6f26e6e
Rafliram <keerouk.ink@gmail.com>
2020-07-16 23:46:13 +0700

    (+) SignIn SignUp Main Layout

---
+++ app/src/main/java/com/example/crud/auth/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.crud.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.crud.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
